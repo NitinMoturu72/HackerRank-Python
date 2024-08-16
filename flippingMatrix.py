48db831fded27a75e6caf740cdecae14c0f5f683
Snehita Moturu <nitinmoturu72@gmail.com>
2024-08-15 18:11:12 -0700

    15-Aug24 Matrix Flipping updated

--- flippingMatrix.py
+++ flippingMatrix.py
@@ -1,9 +1,16 @@
+
+
+
 def flippingMatrix(matrix):
     # Write your code here
     n = len(matrix)
-    max_ulq = 0
-    for i in range(n):
-        for j in range(n-1-i):
-            max_element = max(matrix[i][j], matrix[n-1-i][j], matrix[i][n-1-j], matrix[n-1-i][n-1-j])
-            max_ulq += max_element
-    return max_ulq
+    sumulq = 0
+    for i in range(n//2):
+        for j in range(n//2):
+            # print(matrix[i][j], i, j)
+            # print(max(matrix[i][j], matrix[i][n-j-1], matrix[n-i-1][j], matrix[n-i-1][n-j-1]))
+            sumulq += max(matrix[i][j], matrix[i][n-j-1], matrix[n-i-1][j], matrix[n-i-1][n-j-1])
+            
+    return sumulq
+
+print(flippingMatrix([[112,42,83,119],[56,125,56,49],[15,78,101,43],[62,98,114,108]]))
